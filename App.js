0d08a5abf23420fa616ef673ad0bb2717f512c21
Sidhant Acharya <60935174+cdhant7@users.noreply.github.com>
2023-06-28 23:28:27 +0545

    Update App.js

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import './App.css';
+import Cards from './components/cards/cards';
+import CountryPicker from './components/countrypicker/countrypicker';
+import Charts from './components/charts/charts';
+import { fetchData } from './api';
+import styles from './App.module.css'
 
 class App extends React.Component {
+ 
+  async componentDidMount(){
+    const data= await fetchData()
+
+    console.log(data)
+  }
+
   render (){ 
     return(
-    <div className="App">
-     <h1>hello</h1>
+    <div className={styles.container}>
+     <Cards/>
+     <CountryPicker/>
+     <Charts/>
     </div>
     )
   };
