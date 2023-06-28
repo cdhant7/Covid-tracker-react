107a4fc38649f6aeffdf303b688eeebfbde9109c
Sidhant Acharya <60935174+cdhant7@users.noreply.github.com>
2023-06-28 23:33:03 +0545

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import axios from "axios";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const url='https://api.covidtracking.com/v1/us/daily.json'
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+export const fetchData=async () =>{
+    try {
+        const response=await axios.get(url);
+        
+        return response;
+
+    } catch (error) {
+        
+    }
+}
