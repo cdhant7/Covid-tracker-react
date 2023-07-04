413699f5bb4314f75ae6608f4beeab3d2471aca9
Sidhant Acharya <60935174+cdhant7@users.noreply.github.com>
2023-07-04 16:15:40 +0545

    Update App.js

--- App.js
+++ App.js
@@ -1,33 +1,12 @@
-import React from 'react';
-import './App.css';
-import Cards from './components/cards/cards';
-import CountryPicker from './components/countrypicker/countrypicker';
-import Charts from './components/charts/charts';
-import { fetchData } from './api';
-import styles from './App.module.css'
+import React from 'react'
+import Covid from './components/covid'
 
-class App extends React.Component {
- state={
-    data:{},
-  }
-
- 
-  async componentDidMount(){
-    const data= await fetchData()
-
-    this.setState({data:fetchData})
-  }
-
-  render (){ 
-   const {data}=this.state;
-    return(
-    <div className={styles.container}>
-     <Cards data={data}/>
-     <CountryPicker/>
-     <Charts/>
-    </div>
-    )
-  };
+function App() {
+  return (
+    <>
+  <Covid/>
+  </>
+  )
 }
 
-export default App;
+export default App
